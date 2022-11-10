8aef7c375441ce588dd0ccf29ac8e780b3a3586a
kmanhas <94873627+Karan-Manhas@users.noreply.github.com>
2022-11-10 20:15:42 +0000

    Update script

--- onelinescript.js
+++ onelinescript.js
@@ -5,3 +5,21 @@ document.designMode = 'on';
 const merge =[...array1, ...array2];
 
 //Generate Random Colour
+document.body.style.background=`#${Math.floor(Math.random()*0xffffff).toString(16)}`
+
+//Save to Clipboard
+const button = document.querySelector("button");
+copy = ("Thanks for clicking")
+button.addEventListener("click", () => {
+    alert("Copied to Clipboard ✅");
+});
+
+const copy = text => navigator.clipboard.writeText(text)
+
+// Console log table (When yyou have an array you can print it out nicely in console)
+const videos =[
+    {name: "Tailwind", views: 100},
+    {name: "Tailwind2", views: 200},
+    {name: "Tailwind3", views: 900}
+]
+console.table(videos)
